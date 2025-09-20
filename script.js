e43fd24c8d83c93921bb00a6ecd15ad3bc010e5c
Dev-13552 <devrathour110@gmail.com>
2025-09-20 16:55:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,12 @@ function createTable() {
     //Write your code here
 	const table = document.querySelector("#myTable")
 	let rows = Number(prompt("Input Number of rows"))
-		if(isNaN(rows){
-		return;
+		if(isNaN(rows)){
+			return;
+		}
+		if(rows <= 0){
+		alert("Enter valid Number")
+		return
 	}
 	let cols = Number(prompt("Input Number of columns"))
 
@@ -11,7 +15,7 @@ function createTable() {
 		return;
 	}
 
-	if(rows <= 0 || cols <= 0){
+	if(cols <= 0){
 		alert("Enter valid Number")
 		return
 	}
