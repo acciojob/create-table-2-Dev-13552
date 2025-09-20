dad3bee427bc7b1db301aac8ce3c0da21b448e0f
Dev-13552 <devrathour110@gmail.com>
2025-09-20 16:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
 function createTable() {
     //Write your code here
-  
+	const table = document.querySelector("#myTable")
+	let rows = Number(prompt("Input Number of rows"))
+	let cols = Number(prompt("Input Number of columns"))
+
+	if(isNaN(rows) || isNaN(cols)){
+		return;
+	}
+
+	if(rows <= 0 || cols <= 0){
+		alert("Enter valid Number")
+		return
+	}
+
+	for(let i = 0; i < rows; i++){
+		let row = document.createElement('tr')
+		for(let j = 0; j < cols; j++){
+			let col = document.createElement('td')
+			col.innerHTML = `Row-${i} Column-${j}`
+			row.appendChild(col)
+		}
+		table.appendChild(row)
+	}
 }
